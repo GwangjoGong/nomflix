a934459cee01a15643d7a8d213bd8ca9555a493f
dwc05101 <dwc05101@kaist.ac.kr>
2020-11-10 00:47:46 +0900

    basename config

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
-  Switch
-} from 'react-router-dom'
-import Home from 'Routes/Home'
-import Search from 'Routes/Search'
-import TV from 'Routes/TV'
-import Header from 'Components/Header'
-import Detail from 'Routes/Detail'
+  Switch,
+} from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+import Header from "Components/Header";
+import Detail from "Routes/Detail";
 
 const router = () => (
-  <Router>
+  <Router basename="/">
     <Header />
     <Switch>
-      <Route path='/' exact component={Home} />
-      <Route path='/tv' component={TV} />
-      <Route path='/search' component={Search} />
-      <Route path='/movie/:id' component={Detail} />
-      <Route path='/show/:id' component={Detail} />
-      <Redirect from='*' to='/' />
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
-)
+);
 
-export default router
+export default router;
